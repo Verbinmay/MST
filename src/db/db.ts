1afbbbd956f072fb6c2beb8795f039e4a433ef93
Mark Maistrenko <markomaistrenko@gmail.com>
2024-11-25 15:02:39 +0400

    add in db blogs

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,18 +1,23 @@
+import { BlogViewModel } from "../types/blogs/BlogViewModel.type";
 import { VideoType } from "../types/videos/Video.type";
 
 export type DBType = {
   videos: Array<VideoType>;
+  blogs: Array<BlogViewModel>;
 };
 
 export const db: DBType = {
   videos: [],
+  blogs: [],
 };
 
 export const setDB = (dataset?: Partial<DBType>) => {
   if (!dataset) {
     db.videos = [];
+    db.blogs = [];
     return;
   }
 
   db.videos = dataset.videos || db.videos;
+  db.blogs = dataset.blogs || db.blogs;
 };
