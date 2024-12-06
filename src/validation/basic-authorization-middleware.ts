0dd1686d1d8afccad282337ca075f83b657c9451
Mark Maistrenko <markomaistrenko@gmail.com>
2024-12-06 16:19:59 +0400

    fix:0

---
+++ src/validation/basic-authorization-middleware.ts
@@ -0,0 +1,30 @@
+import { config } from "dotenv";
+import { NextFunction, Request, Response } from "express";
+
+config();
+export const basicAuthorizationMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authorization = req.headers["authorization"];
+    if (authorization) {
+      const [type, token] = authorization.split(" ");
+      if (type === "Basic" && token) {
+        const decoded = Buffer.from(token, "base64").toString("utf-8");
+        const [username, password] = decoded.split(":");
+        if (
+          username === process.env.BASIC_AUTH_LOGIN &&
+          password === process.env.BASIC_AUTH_PASSWORD
+        ) {
+          next();
+          return;
+        }
+      }
+    }
+    res.sendStatus(401);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+};
